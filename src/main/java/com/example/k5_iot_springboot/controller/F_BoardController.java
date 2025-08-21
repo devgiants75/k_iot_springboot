6b1ed445dfef558397c2af15032cf476f51f6eb6
devgiants75 <devgiants75@naver.com>
2025-08-21 10:50:23 +0900

    20250821 DATETIME

---
+++ src/main/java/com/example/k5_iot_springboot/controller/F_BoardController.java
@@ -0,0 +1,13 @@
+package com.example.k5_iot_springboot.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/boards")
+@RequiredArgsConstructor
+@Validated
+public class F_BoardController {
+}
---
+++ src/main/java/com/example/k5_iot_springboot/service/impl/F_BoardServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.k5_iot_springboot.service.impl;
+
+import com.example.k5_iot_springboot.service.F_BoardService;
+
+public class F_BoardServiceImpl implements F_BoardService {
+}
