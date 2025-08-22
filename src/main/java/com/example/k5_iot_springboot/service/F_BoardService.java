84d328884c1ef3864f33418d2e0cfb383d7e8f42
devgiants75 <devgiants75@naver.com>
2025-08-22 12:47:16 +0900

    20250822 Pagination

--- src/main/java/com/example/k5_iot_springboot/service/F_BoardService.java
+++ src/main/java/com/example/k5_iot_springboot/service/F_BoardService.java
@@ -6,6 +6,7 @@ import com.example.k5_iot_springboot.dto.ResponseDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public interface F_BoardService {
     ResponseDto<List<BoardResponseDto.SummaryResponse>> getAllBoards();
     ResponseDto<BoardResponseDto.DetailResponse> updateBoard(Long boardId, BoardRequestDto.@Valid UpdateRequest request);
 
-    ResponseDto<BoardResponseDto.PageResponse> getBoardsPage(@Min(0) int page, @Min(1) @Max(100) int size, String[] sort);
+//    ResponseDto<BoardResponseDto.PageResponse> getBoardsPage(@Min(0) int page, @Min(1) @Max(100) int size, String sort);
+    ResponseDto<BoardResponseDto.PageResponse> getBoardsPage(Pageable pageable);
     ResponseDto<BoardResponseDto.SliceResponse> getBoardsByCursor(Long cursorId, @Min(1) @Max(100) int size);
 }
