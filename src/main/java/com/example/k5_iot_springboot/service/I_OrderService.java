4d47e7eee4d28cf64c60070e09d5c0462e8fa9ae
devgiants75 <devgiants75@naver.com>
2025-09-03 12:33:36 +0900

    20250903 주문 관리 시스템Order

--- src/main/java/com/example/k5_iot_springboot/service/I_OrderService.java
+++ src/main/java/com/example/k5_iot_springboot/service/I_OrderService.java
@@ -13,5 +13,5 @@ public interface I_OrderService {
     ResponseDto<OrderResponse.Detail> create(UserPrincipal userPrincipal, OrderRequest.OrderCreateRequest req);
     ResponseDto<OrderResponse.Detail> approve(UserPrincipal userPrincipal, Long orderId);
     ResponseDto<OrderResponse.Detail> cancel(UserPrincipal userPrincipal, Long orderId);
-    ResponseDto<List<OrderResponse.Detail>> search(Long userId, OrderStatus status, LocalDateTime from, LocalDateTime to);
+    ResponseDto<List<OrderResponse.Detail>> search(UserPrincipal userPrincipal, Long userId, OrderStatus status, LocalDateTime from, LocalDateTime to);
 }
