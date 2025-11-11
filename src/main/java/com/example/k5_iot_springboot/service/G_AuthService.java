e4db06d1e475b56af0122cc602d65bcb44b04d20
devgiants75 <devgiants75@naver.com>
2025-11-11 11:11:01 +0900

    20251111 refresh token

--- src/main/java/com/example/k5_iot_springboot/service/G_AuthService.java
+++ src/main/java/com/example/k5_iot_springboot/service/G_AuthService.java
@@ -5,6 +5,7 @@ import com.example.k5_iot_springboot.dto.G_Auth.request.SignUpRequest;
 import com.example.k5_iot_springboot.dto.G_Auth.response.SignInResponse;
 import com.example.k5_iot_springboot.dto.J_Mail.MailRequest;
 import com.example.k5_iot_springboot.dto.ResponseDto;
+import com.example.k5_iot_springboot.security.UserPrincipal;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
@@ -13,4 +14,8 @@ public interface G_AuthService {
     ResponseDto<SignInResponse> signIn(@Valid SignInRequest req, HttpServletResponse response);
 
     void resetPassword(MailRequest.@Valid PasswordReset req);
+
+    String refreshAccessToken(String refreshToken);
+
+    void deleteRefreshToken(UserPrincipal userPrincipal);
 }
